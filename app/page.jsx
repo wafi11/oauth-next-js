fdb4aaff934fcf690b6767037d6b14a6254e3187
unknown <wafiwafiwafi90@gmail.com>
2024-04-04 23:23:48 -0700

    finallly

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,24 @@
 import { ApiJikan } from "./api/jikan-api";
 import Banner from "../components/main/Banner";
 import MovieList from "../components/main/movieList";
+import { cookies } from "next/headers";
+
+export function getCookieData() {
+  const cookieData = cookies().getAll();
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(cookieData);
+    }, 1000)
+  );
+}
 
 export default async function Home() {
+  const cookieData = await getCookieData();
   const TopAnime = await ApiJikan("top/anime", "limit=8");
   const result = await TopAnime.data;
   const TopManga = await ApiJikan("top/anime", "page=2");
   const result2 = await TopManga.data;
+
   return (
     <>
       <Banner item={result2} />
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,8 +1,19 @@
 // "use client";
 import React from "react";
 import SearchClient from "./SearchClient";
+import { cookies } from "next/headers";
 
-const Search = () => {
+export function getCookieData() {
+  const cookieData = cookies().getAll();
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(cookieData);
+    }, 1000)
+  );
+}
+
+const Search = async () => {
+  const cookieData = await getCookieData();
   return <SearchClient />;
 };
 export default Search;
